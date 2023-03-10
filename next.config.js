869fa2827f96a24f680de7adaa0cb8df78413c3d
a2d7cc <a2d7cc@gmail.com>
2023-03-10 10:14:52 +0100

    Init Next project

--- next.config.js
+++ next.config.js
@@ -1,7 +1,65 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	poweredByHeader: false,
+	optimizeFonts: false,
+
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+		APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
+	},
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `http://localhost:4200/api/:path*`,
+			},
+			{
+				source: '/uploads/:path*',
+				destination: `http://localhost:4200/uploads/:path*`,
+			},
+		]
+	},
+
+	webpack(config, options) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			resourceQuery: /inline/, // only use svgr to load svg if path ends with *.svg?svgr
+			use: [
+				{
+					loader: '@svgr/webpack',
+					options: {
+						prettier: false,
+						svgo: true,
+						svgoConfig: {
+							plugins: [
+								{
+									name: 'preset-default',
+									params: {
+										overrides: { removeViewBox: false },
+									},
+								},
+							],
+						},
+						titleProp: true,
+						icon: true,
+					},
+				},
+			],
+		})
+
+		config.module.rules.push({
+			test: /\.svg$/i,
+			loader: 'next-image-loader',
+			issuer: { not: /\.(css|scss|sass)$/ },
+			dependency: { not: ['url'] },
+			resourceQuery: { not: [/inline/] }, // Ignore this rule if the path ends with *.svg?svgr
+			options: { basePath: '', assetPrefix: '' },
+		})
+
+		return config
+	},
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,112 @@
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+const accent = '#722ed1'
+const lightAccent = '#f9f0ff'
+
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./app/components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		fontFamily: {
+			primary: 'Orbitron',
+			secondary: 'Rajdhani',
+			tertiary: 'Aldrich',
+		},
+		container: {
+			padding: {
+				DEFAULT: '15px',
+			},
+		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '960px',
+			xl: '1200px',
+		},
+		colors: {
+			primary: '#0a0a0a',
+			accent: '#B809C3',
+			black: colors.black,
+			white: colors.white,
+		},
+		extend: {
+			spacing: {
+				0.5: '0.12rem',
+				layout: '2.75rem',
+			},
+			fontSize: {
+				'2lg': '1.38rem',
+			},
+			borderRadius: {
+				image: '0.5rem',
+				layout: '0.8rem',
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'ease-in-out',
+			},
+			transitionDuration: {
+				DEFAULT: '200ms',
+			},
+			keyframes: {
+				fade: {
+					from: { opacity: 0 },
+					to: { opacity: 1 },
+				},
+				scaleIn: {
+					'0%': {
+						opacity: 0,
+						transform: 'scale(0.9)',
+					},
+					'50%': {
+						opacity: 0.3,
+					},
+					'100%': {
+						opacity: 1,
+						transform: 'scale(1)',
+					},
+				},
+			},
+			animation: {
+				fade: 'fade .5s ease-in-out',
+				scaleIn: 'scaleIn .35s ease-in-out',
+			},
+			zIndex: {
+				1: '1',
+				2: '2',
+				3: '3',
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/aspect-ratio'),
+		plugin(({ addComponents, theme, addUtilities }) => {
+			addComponents({}),
+				addUtilities({
+					'.text-shadow': {
+						textShadow: '1px 1px rgba(0, 0, 0, 0.4)',
+					},
+
+					'.outline-border-none': {
+						outline: 'none',
+						border: 'none',
+					},
+
+					'.flex-center-between': {
+						display: 'flex',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+					},
+
+					'.image-like-bg': {
+						objectPosition: 'center',
+						objectFit: 'cover',
+						pointerEvents: 'none',
+					},
+				})
+		}),
+	],
+}
